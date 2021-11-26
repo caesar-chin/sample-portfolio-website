267d109981c16c8babd48bf96d0838708dc824bd
caesar-chin <56019561+caesar-chin@users.noreply.github.com>
2021-11-25 21:05:55 -0500

    First Commit

---
+++ scripts.js
@@ -0,0 +1,107 @@
+AOS.init({});
+
+var spy = new Gumshoe(".navitems a");
+
+//Typing function
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 175 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 3;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+
+//black to white transition
+$(document).ready(function () {
+  setTimeout(function () {
+    $("body").css("background-color", "darkgrey");
+  }, 2750);
+});
+
+$(document).ready(function () {
+  setTimeout(function () {
+    $(".typewrite").css("color", "black");
+  }, 2750);
+});
+
+//scroll effect
+$(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        500,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
+
+function myFunction() {
+  var x = document.getElementsByClassName("navitems")[0];
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
